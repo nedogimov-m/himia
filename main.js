2a6c385ca8f38f0844183d9fd525f53745ffbc9f
Marat Nedogimov <101347916+nedogimov-m@users.noreply.github.com>
2022-12-16 23:34:56 +0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <img id="gdz" style="display: none; position: relative" src="" width="50%" height="50%" alt="piska bobra"/>
+    <img id="gdz" style="display: none; position: relative" src="" width="95%" height="95%" alt="piska bobra"/>
     <br><br>
     <label for="glavat">Глава: </label>
     <input class="input-field col s6" type="text" id="glava" name="glava"><br><br>
