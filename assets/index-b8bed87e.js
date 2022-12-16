7cdb47fcca17c4e48b781037147e055a5959a54d
Marat Nedogimov <101347916+nedogimov-m@users.noreply.github.com>
2022-12-16 23:35:43 +0500

    Update index-b8bed87e.js

--- assets/index-b8bed87e.js
+++ assets/index-b8bed87e.js
@@ -1,6 +1,6 @@
 (function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();document.querySelector("#app").innerHTML=`
   <div>
-    <img id="gdz" style="display: none; position: relative" src="" width="50%" height="50%" alt="piska bobra"/>
+    <img id="gdz" style="display: none; position: relative" src="" width="95%" height="95%" alt="piska bobra"/>
     <br><br>
     <label for="glavat">Глава: </label>
     <input class="input-field col s6" type="text" id="glava" name="glava"><br><br>
