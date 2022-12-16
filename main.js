5a1fd029cb7e49bff34b51836ded7da168080abb
nedogimov-m <nedogimoff@bebracraft.ru>
2022-12-16 23:23:36 +0500

    first-commit

---
+++ main.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <img id="gdz" style="display: none; position: relative" src="" width="50%" height="50%" alt="piska bobra"/>
+    <br><br>
+    <label for="glavat">Глава: </label>
+    <input class="input-field col s6" type="text" id="glava" name="glava"><br><br>
+    <label for="nomert">Номер задачи: </label>
+    <input type="text" id="nomer" name="nomer"><br><br>
+    <a class="waves-effect waves-light btn" onclick="showImg('glava', 'nomer')">Открыть</a>
+  </div>
+`
